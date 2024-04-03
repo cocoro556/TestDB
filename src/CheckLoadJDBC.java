cf040d7baa266caf2667a11391505762e1b9d1ff
cocoro <yuto.nakayama0@gmail.com>
2024-04-03 20:34:07 +0900

    first commit

---
+++ src/CheckLoadJDBC.java
@@ -0,0 +1,12 @@
+public class CheckLoadJDBC {
+	public static void main(String[] args) {
+		String msg = "";
+		try {
+			Class.forName("com.mysql.cj.jdbc.Driver");
+			msg = "ドライバのロードに成功しました";
+		} catch (ClassNotFoundException e) {
+			msg = "ドライバのロードに失敗しました";
+		}
+		System.out.println(msg);
+	}
+}
